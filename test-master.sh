0ed01d5c181cc1abef872eb2cc2df082766781ae
Thomas Mueller <thomas.mueller@tmit.eu>
2012-08-31 23:00:50 +0200

    fixing test-master.sh

--- test-master.sh
+++ test-master.sh
@@ -7,12 +7,6 @@
 #
 
 #
-# setup
-#
-gem install bundler
-bundle install
-
-#
 # first start the vm(s)
 #
 cd Vagrant
@@ -20,9 +14,27 @@ vagrant up master_on_apache
 #vagrant up master_on_lighttpd
 
 #
+# setup cucumber - I use rvm here
+#
+source "$HOME/.rvm/scripts/rvm"
+rvm ruby-1.9.2-p290
+rvm gemset use oc_acceptance
+
+# let's assume bundler is installed
+#gem install bundler
+bundle install
+
+#
 # fire the bdd test suite
 #
-bundle exec cucumber HOST=33.33.33.10 features
+cd ..
+bundle exec cucumber -f junit -o ./logs/apache HOST=33.33.33.10 features
 #bundle exec cucumber HOST=33.33.34.10 features
 
+#
+# bring down the vm
+#
+cd Vagrant
+#vagrant halt master_on_apache
+#vagrant up master_on_lighttpd
 
