b1366d340b80105dbce7c763ea7714fae225f274
Jakob Sack <mail@jakobsack.de>
2012-12-28 23:07:10 +0100

    first steps with chef

---
+++ Vagrant/solo.rb
@@ -0,0 +1,5 @@
+file_cache_path File.dirname( __FILE__ )
+# file_cache_path "/home/jakob/projekte/owncloud-acceptance/Vagrant"
+cookbook_path [File.join( File.dirname( __FILE__ ), "cookbooks" )]
+# cookbook_path ["/home/jakob/projekte/owncloud-acceptance/Vagrant/cookbooks"]
+
