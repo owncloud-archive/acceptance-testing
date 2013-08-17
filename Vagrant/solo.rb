eab2d79f5ebd43887d3bc230f059e09395eaa24b
Jakob Sack <mail@jakobsack.de>
2013-08-17 16:18:21 +0200

    Remove old, deprecated cookbooks

--- Vagrant/solo.rb
+++
@@ -1,5 +0,0 @@
-file_cache_path File.dirname( __FILE__ )
-# file_cache_path "/home/jakob/projekte/owncloud-acceptance/Vagrant"
-cookbook_path [File.join( File.dirname( __FILE__ ), "cookbooks" )]
-# cookbook_path ["/home/jakob/projekte/owncloud-acceptance/Vagrant/cookbooks"]
-
